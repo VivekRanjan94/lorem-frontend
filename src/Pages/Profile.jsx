10737c932a71f12120a089c7fe988c8b90e2cfaf
Vivek <vivek.ranjaan@gmail.com>
2022-04-24 16:32:03 +0530

    added profile page styles

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -6,10 +6,33 @@ const Profile = () => {
   const { user } = useUser()
   const { logout } = useAuth()
 
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
   return (
-    <div>
-      {user.first_name}
-      <button onClick={logout}>logout</button>
+    <div className='profile'>
+      <div className='profile-details'>
+        <div className='profile-details-detail'>
+          Username:{' '}
+          <div className='profile-details-detail-value'>{user.username}</div>
+        </div>
+        <div className='profile-details-detail'>
+          First Name:{' '}
+          <div className='profile-details-detail-value'>{user.first_name}</div>
+        </div>
+        <div className='profile-details-detail'>
+          Last Name:{' '}
+          <div className='profile-details-detail-value'>{user.last_name}</div>
+        </div>
+        <div className='profile-details-detail'>
+          Type:{' '}
+          <div className='profile-details-detail-value'>
+            {capitalize(user.type)}
+          </div>
+        </div>
+      </div>
+      <button onClick={logout}>Log Out</button>
     </div>
   )
 }
